1174dea0727338739cfbcc24c63db1fc9f920c9e
Adrian Heyl <adrian.heyl@gmail.com>
2018-12-14 02:53:25 +0100

    Added example character sheets

--- src/main.py
+++ src/main.py
@@ -5,8 +5,9 @@ from window import Window
 
 window = Window()
 
-window.add_unit('Walter', ['Druide'], [2], -6, 19, 14)
-window.add_unit('Bob', ['Schurke'], [1], -5, 18, -3)
-window.add_unit('Hans', ['Magier'], [4], -6, 22, 9)
+window.add_unit('Günther', ['Magier'], [4], -6, 22, 9)
+window.add_unit('Walterine', ['Druide'], [2], -7, 19, -3)
+window.add_unit('Bob', ['Paladin'], [1], -5, 18, 14)
+window.add_unit('Haralda', ['Schurke'], [3], -6, 15, 15)
 
 window.start()
