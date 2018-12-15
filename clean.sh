c371890e7b061b9c69997e79cec1cc9239f83ca4
Adrian Heyl <adrian.heyl@gmail.com>
2018-12-15 14:09:04 +0100

    Cleaned Project

--- clean.sh
+++ clean.sh
@@ -6,13 +6,13 @@ count_pyc=`find ./src -maxdepth 1 -name "*.pyc" | wc -l`
 if [ $count_pyc -gt 0 ]; then
     rm ./src/*.pyc
     echo $count .pyc-files removed
-    no_change = False
+    no_change= False
 fi
 
 if [ -e ./src/__pycache__ ]; then
     rm -r ./src/__pycache__
     echo Removed __pycache__
-    no_change = False
+    no_change= False
 fi
 
 if [ no_change ]; then
